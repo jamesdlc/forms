f6f158ed84486d9df7bc66bd579d38b50c7caf32
jamestothe <jamesdelacruz@gmail.com>
2016-10-29 20:03:14 -0700

    added api routes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,12 @@ class TeamsController < ApplicationController
   include TeamsHelper
 
   def index
+    @teams = Team.all
+    render json: @teams, status: :ok #response code 200
+  end
+
+  def show
+    render json: @team
   end
 
   def new
@@ -22,14 +28,17 @@ class TeamsController < ApplicationController
 
   def update
     if @team.update(team_params)
+      head :no_content #status code 204
       flash[:notice] = "Team has been successfully edited."
     else
       flash[:notice] = "Something went wrong."
+      render json: @team.errors, status: :unprocessable_entity #response code 422
     end
   end
 
   def destroy
     if @team.destroy
+      head :no_content #status code 204
       flash[:notice] = "Team has been successfully deleted."
     else
       flash[:notice] = "Something went wrong."
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "teams#index"
+
+  # resources :teams
+
+
+  namespace :api :defaults => {:format => :json} do
+    get "/teams",        to: "teams#index"
+    post "/teams",       to: "teams#create"
+    get "/teams/:id",    to: "teams#show"
+    put "/teams/:id",    to: "teams#update"
+    delete "/teams/:id", to: "teams#destroy"
+  end
+
 end
